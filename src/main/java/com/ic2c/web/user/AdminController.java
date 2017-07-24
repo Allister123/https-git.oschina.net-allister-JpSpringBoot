c476e79cd582ac8e3ec817ab938713b96917407b
Allister <laujip@163.com>
2017-07-24 22:32:22 +0800

    优化代码

--- src/main/java/com/ic2c/web/user/AdminController.java
+++ src/main/java/com/ic2c/web/user/AdminController.java
@@ -11,8 +11,6 @@
  */
 package com.ic2c.web.user;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
