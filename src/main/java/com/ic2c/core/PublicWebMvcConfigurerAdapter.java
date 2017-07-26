5115c5266cbef6c45f300a783e9c85e19625716b
Allister <laujip@163.com>
2017-07-26 17:54:05 +0800

    优化

--- src/main/java/com/ic2c/core/PublicWebMvcConfigurerAdapter.java
+++ src/main/java/com/ic2c/core/PublicWebMvcConfigurerAdapter.java
@@ -38,18 +38,23 @@ public class PublicWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter{
 	@Value("${server.port}")
     private Integer webPort;
 
+	/**
+	 * 拦截器配置
+	 */
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		//拦截所有带有admin的
 		registry.addInterceptor(new AdminInterceptor()).addPathPatterns("/admin/**");
 	}
 
+	
+	//======================================配置将443转向80 start===============================================
 	/**
      * 构建servlet容器的工厂类
      * 将80端口跳转到{@linkplain #webPort}端口
      *
      * @return 内置servlet容器类的工厂实例
-     */
+     *//*
     @Bean
     public EmbeddedServletContainerFactory servletContainer() {
         TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory() {
@@ -68,18 +73,19 @@ public class PublicWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter{
         return factory;
     }
 
-    /**
+    *//**
      * 创建tomcat连接器。
      * 该连接器将会接收http的80端口的访问，并且重定向到指定的端口上去，{@linkplain #webPort}
      *
      * @return tomcat连接器
-     */
+     *//*
     private Connector createConnector() {
         final Connector connector = new Connector();
 
         connector.setPort(80);
         connector.setRedirectPort(webPort);
         return connector;
-    }
+    }*/
 	
+	//======================================配置将443转向80 end===============================================
 }
