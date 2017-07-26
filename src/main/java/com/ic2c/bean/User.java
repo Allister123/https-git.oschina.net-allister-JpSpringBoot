5081fccc1c997ed9e65565d96ba4a55d384da354
Allister <laujip@163.com>
2017-07-26 17:56:22 +0800

    优化

--- src/main/java/com/ic2c/bean/User.java
+++ src/main/java/com/ic2c/bean/User.java
@@ -1,28 +1,25 @@
 /**
  * 
  */
-
 package com.ic2c.bean;
 
 import java.util.Date;
 
+
 /**
  * 
  * 
- * User 
- * 创建人:刘继鹏 
- * 时间：2016年1月7日-下午10:20:47
+ * User 创建人:刘继鹏 时间：2016年1月7日-下午10:20:47
  * 
  * @version 1.0.0
- * 
+ *          
  */
-public class User implements java.io.Serializable{
-
+public class User implements java.io.Serializable {
 	/**
 	 * serialVersionUID:TODO
+	 * 
 	 * @since 1.0.0
 	 */
-	
 	private static final long serialVersionUID = 1L;
 	// 主键ID
 	private Integer id;
@@ -52,185 +49,263 @@ public class User implements java.io.Serializable{
 	private Date birthday;
 	// 头像
 	private String headerPic;
-	//问题一
+	// 问题一
 	private Integer questionOne;
-	//问题二
+	// 问题二
 	private Integer questionTwo;
-	//问题一答案
+	// 问题一答案
 	private String answerOne;
-	//问题二答案
+	// 问题二答案
 	private String answerTwo;
-	
 	/**
 	 * 临时字段（用户导出Excel）
 	 */
-	private String deleteName = "未删除";//删除状态
+	private String deleteName = "未删除";// 删除状态
 	private String forbidenName = "允许";
-
+	
+	
+	
 	public Integer getId() {
 		return id;
 	}
-
+	
+	
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
+	
+	
+	
 	public String getUsername() {
 		return username;
 	}
-
+	
+	
+	
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
+	
+	
+	
 	public String getPassword() {
 		return password;
 	}
-
+	
+	
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	
+	
 	public Date getCreateTime() {
 		return createTime;
 	}
-
+	
+	
+	
 	public void setCreateTime(Date createTime) {
 		this.createTime = createTime;
 	}
-
+	
+	
+	
 	public Date getUpdateTime() {
 		return updateTime;
 	}
-
+	
+	
+	
 	public void setUpdateTime(Date updateTime) {
 		this.updateTime = updateTime;
 	}
-
+	
+	
+	
 	public Integer getForbiden() {
 		return forbiden;
 	}
-
+	
+	
+	
 	public void setForbiden(Integer forbiden) {
 		this.forbiden = forbiden;
 	}
-
+	
+	
+	
 	public Integer getIsDelete() {
 		return isDelete;
 	}
-
+	
+	
+	
 	public void setIsDelete(Integer isDelete) {
 		this.isDelete = isDelete;
 	}
-
+	
+	
+	
 	public String getEmail() {
 		return email;
 	}
-
+	
+	
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	
+	
+	
 	public String getTelephone() {
 		return telephone;
 	}
-
+	
+	
+	
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
-
+	
+	
+	
 	public Integer getAge() {
 		return age;
 	}
-
+	
+	
+	
 	public void setAge(Integer age) {
 		this.age = age;
 	}
-
+	
+	
+	
 	public String getAddress() {
 		return address;
 	}
-
+	
+	
+	
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
+	
+	
+	
 	public Integer getMale() {
 		return male;
 	}
-
+	
+	
+	
 	public void setMale(Integer male) {
 		this.male = male;
 	}
-
+	
+	
+	
 	public Date getBirthday() {
 		return birthday;
 	}
-
+	
+	
+	
 	public void setBirthday(Date birthday) {
 		this.birthday = birthday;
 	}
-
+	
+	
+	
 	public String getHeaderPic() {
 		return headerPic;
 	}
-
+	
+	
+	
 	public void setHeaderPic(String headerPic) {
 		this.headerPic = headerPic;
 	}
-
+	
+	
+	
 	public Integer getQuestionOne() {
 		return questionOne;
 	}
-
+	
+	
+	
 	public void setQuestionOne(Integer questionOne) {
 		this.questionOne = questionOne;
 	}
-
+	
+	
+	
 	public Integer getQuestionTwo() {
 		return questionTwo;
 	}
-
+	
+	
+	
 	public void setQuestionTwo(Integer questionTwo) {
 		this.questionTwo = questionTwo;
 	}
-
+	
+	
+	
 	public String getAnswerOne() {
 		return answerOne;
 	}
-
+	
+	
+	
 	public void setAnswerOne(String answerOne) {
 		this.answerOne = answerOne;
 	}
-
+	
+	
+	
 	public String getAnswerTwo() {
 		return answerTwo;
 	}
-
+	
+	
+	
 	public void setAnswerTwo(String answerTwo) {
 		this.answerTwo = answerTwo;
 	}
-
+	
+	
+	
 	public String getDeleteName() {
 		return deleteName;
 	}
-
+	
+	
+	
 	public void setDeleteName(String deleteName) {
 		this.deleteName = deleteName;
 	}
-
+	
+	
+	
 	public String getForbidenName() {
 		return forbidenName;
 	}
-
+	
+	
+	
 	public void setForbidenName(String forbidenName) {
 		this.forbidenName = forbidenName;
 	}
-
+	
+	
+	
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-
-	
-	
 }
